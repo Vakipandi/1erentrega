7502d4e847eea4d917545d658cdb51adc57ce1ad
mau <mauriciomamani306@gmail.com>
2023-07-03 21:33:24 -0500

    1er entrega

--- src/app.js
+++ src/app.js
@@ -1,34 +1,14 @@
-import ProductManager, { arrayProd } from "./productManager.js";
+import productsRouter from './router/products.router.js'
+import cartsRouter from './router/carts.router.js'
+import __dirname from './utils.js';
 import express from "express";
-
-const manager = new ProductManager("./files/products.json");
-
-const newProducts = async () => {
-  for (let i = 0; i < arrayProd.length; i++) {
-    const element = arrayProd[i];
-    await manager.addProduct(element);
-  }
-};
-
-newProducts();
 
 const app = express();
 
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json())
+app.use(express.urlencoded({extended: true}));
 
-app.get("/products", async (req, res) => {
-  const { limit } = req.query;
-  let products = await manager.getProducts();
-  if (limit) {
-    products = products.slice(0, Number(limit));
-  }
-  res.send({ products });
-});
-
-app.get("/products/:pid", async (req, res) => {
-  const productId = Number(req.params.pid);
-  const product = await manager.getProductsById(productId);
-  res.send(product);
-});
+app.use('/api/products', productsRouter)
+app.use('/api/carts', cartsRouter)
 
 app.listen(8080, console.log("Listening on 8080"));
